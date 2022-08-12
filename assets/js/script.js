b5ba8d26386cc6df6870bc5ba806675ad97a5ef8
Divyapriyadharshini Babu <divyapriyadharshinibabu@gmail.com>
2022-08-12 14:38:06 -0700

    Use different prompts for each character set

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,8 @@
 // Different characters allowed for password
-var charsetArray = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", " \!\"\#$%&'()*+,-./:;<=>?@[\]^_`{|}~" ];
-
-var allcharSet = '';
-
-for (var i=0; i<charsetArray.length ; i++){
-  allcharSet += charsetArray[i];
-};
-
-var lowerUpper = charsetArray[0].concat(charsetArray[1]);
-var lowerNumeric = charsetArray[0].concat(charsetArray[2]);
-var lowerSpecial = charsetArray[0].concat(charsetArray[3]);
-var upperNumeric = charsetArray[1].concat(charsetArray[2]);
-var upperSpecial = charsetArray[1].concat(charsetArray[3]);
-var numericSpecial = charsetArray[2].concat(charsetArray[3]);
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var specialCharacters = " \!\"\#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 function generatePassword() {
 
@@ -22,41 +12,37 @@ function generatePassword() {
   if ((lengthOfPassword >= 8) && (lengthOfPassword <= 128)) {
     console.log("Good job!! You have selected the right option");
 
-    // Get the password combination
-    var charactertype = window.prompt("Select atleast one type of characters to be included: \n" + 
-    "1. Lowercase[a-z] \n 2. Uppercase[A-Z] \n 3. Numeric[0-9] \n 4. Special characters \!\"\#$%&'()*+,-./:;<=>?@[\]^_`{|}~  \n" +
-    "5. Lowercase, Uppercase, Numeric and Special characters \n 6. Uppercase and Lowercase \n 7. Lowercase and Numeric \n 8. Lowercase and Special character \n" + 
-    "9. Uppercase and Numeric \n 10. Uppercase and Special characters \n 11. Numeric and Special characters \n ");
-    
-    if ((charactertype >= 1) && (charactertype <= 11)) {  
-      return randomGenerator(lengthOfPassword, charactertype); 
-    } else {
-      window.alert("Select one from the option (1-11)");
-    }
+    var containsUpper = window.confirm( "Does the password contain Uppercase[A-Z]?\nOK - Yes\nCancel - No");
+    var containsLower = window.confirm( "Does the password contain Lowercase[a-z]?\nOK - Yes\nCancel - No");
+    var containsNumeric = window.confirm( "Does the password contain Numeric[0-9]?\nOK - Yes\nCancel - No");
+    var containsSpecialCharacter = window.confirm( "Does the password contain Special characters?\nOK - Yes\nCancel - No");
 
+    var charSet = "";
+    if (!containsUpper && !containsLower && !containsNumeric && !containsSpecialCharacter) {
+      window.alert("You must choose a criteria!");
+    } else {
+      // Get the password combination
+      if (containsUpper){
+        charSet = charSet.concat(upperCase);
+      }
+      if (containsLower) {
+        charSet = charSet.concat(lowerCase);
+      }
+      if (containsNumeric) {
+        charSet = charSet.concat(numeric);
+      }
+      if (containsSpecialCharacter) {
+        charSet = charSet.concat(specialCharacters);
+      }
+      return randomGenerator(lengthOfPassword, charSet);
+    }
   } else {
     window.alert("OOPS!!! choose between 8 and 128");
   }
-
   return "";
 }
 
-function randomGenerator(len, charactertypePassword){
-  var characterType = {
-    // Properties are made up of key-value pairs
-    1 : charsetArray[0],
-    2 : charsetArray[1],
-    3 : charsetArray[2],
-    4 : charsetArray[3],
-    5 : allcharSet,
-    6 : lowerUpper,
-    7 : lowerNumeric,
-    8 : lowerSpecial,
-    9 : upperNumeric,
-    10 : upperSpecial,
-    11 : numericSpecial
-  };
-  var charSet = characterType[charactertypePassword];
+function randomGenerator(len, charSet){
   var randomPassword = '';
   for (var i = 0; i < len; i++) {
     // Use random to get random positions from the password combination allowed.
